ab75db4a57f81d28df5ccbc20eb4cfdd5f666e69
Moritz Heinemann <moritz.heinemann@visus.uni-stuttgart.de>
2021-10-20 14:06:15 +0200

    add download url option

--- cmake/External_download.cmake
+++ cmake/External_download.cmake
@@ -10,13 +10,15 @@ include(FetchContent)
 #
 function(external_download TARGET)
   # Parse arguments
-  set(ARGS_ONE_VALUE GIT_TAG GIT_REPOSITORY SOURCE_SUBDIR)
+  set(ARGS_ONE_VALUE GIT_TAG GIT_REPOSITORY URL URL_HASH SOURCE_SUBDIR)
   cmake_parse_arguments(args "" "${ARGS_ONE_VALUE}" "" ${ARGN})
 
   # Check for local version
   external_try_get_property(${TARGET} BUILD_TYPE)
   external_try_get_property(${TARGET} GIT_REPOSITORY)
   external_try_get_property(${TARGET} GIT_TAG)
+  external_try_get_property(${TARGET} URL)
+  external_try_get_property(${TARGET} URL_HASH)
   external_try_get_property(${TARGET} SOURCE_SUBDIR)
 
   set(AVAILABLE_VERSION)
@@ -29,32 +31,49 @@ function(external_download TARGET)
   if(DEFINED GIT_TAG)
     list(APPEND AVAILABLE_VERSION ${GIT_TAG})
   endif()
+  if(DEFINED URL)
+    list(APPEND AVAILABLE_VERSION ${URL})
+  endif()
+  if(DEFINED URL_HASH)
+    list(APPEND AVAILABLE_VERSION ${URL_HASH})
+  endif()
   if(DEFINED SOURCE_SUBDIR)
     list(APPEND AVAILABLE_VERSION ${SOURCE_SUBDIR})
   endif()
 
   # Check for requested version
-  if(NOT args_GIT_REPOSITORY)
-    message(FATAL_ERROR "No Git repository declared as source")
-  endif()
-
-  set(MESSAGE "${TARGET}: url '${args_GIT_REPOSITORY}'")
-  set(DOWNLOAD_ARGS "GIT_REPOSITORY;${args_GIT_REPOSITORY}")
-
   set(REQUESTED_VERSION)
   list(APPEND REQUESTED_VERSION ${CMAKE_BUILD_TYPE})
-  list(APPEND REQUESTED_VERSION ${args_GIT_REPOSITORY})
 
-  if(args_GIT_TAG)
-    set(MESSAGE "${MESSAGE}, tag '${args_GIT_TAG}'")
-    list(APPEND DOWNLOAD_ARGS "GIT_TAG;${args_GIT_TAG}")
-    list(APPEND REQUESTED_VERSION ${args_GIT_TAG})
-  endif()
+  if(args_GIT_REPOSITORY)
+    set(MESSAGE "${TARGET}: url '${args_GIT_REPOSITORY}'")
+    set(DOWNLOAD_ARGS "GIT_REPOSITORY;${args_GIT_REPOSITORY}")
+    list(APPEND REQUESTED_VERSION ${args_GIT_REPOSITORY})
 
-  # Shallow clone does not work with commit hashes. Trying to detect hash by checking for strlen(tag) >= 40.
-  string(LENGTH "${args_GIT_TAG}" gitTagLength)
-  if(gitTagLength LESS 40)
-    list(APPEND DOWNLOAD_ARGS "GIT_SHALLOW;1")
+    if(args_GIT_TAG)
+      set(MESSAGE "${MESSAGE}, tag '${args_GIT_TAG}'")
+      list(APPEND DOWNLOAD_ARGS "GIT_TAG;${args_GIT_TAG}")
+      list(APPEND REQUESTED_VERSION ${args_GIT_TAG})
+    endif()
+
+    # Shallow clone does not work with commit hashes. Trying to detect hash by checking for strlen(tag) >= 40.
+    string(LENGTH "${args_GIT_TAG}" gitTagLength)
+    if(gitTagLength LESS 40)
+      list(APPEND DOWNLOAD_ARGS "GIT_SHALLOW;1")
+    endif()
+
+  elseif(args_URL)
+    set(MESSAGE "${TARGET}: url '${args_URL}'")
+    set(DOWNLOAD_ARGS "URL;${args_URL}")
+    list(APPEND REQUESTED_VERSION ${args_URL})
+
+    if(args_URL_HASH)
+      set(MESSAGE "${MESSAGE}, hash '${args_URL_HASH}'")
+      list(APPEND DOWNLOAD_ARGS "URL_HASH;${args_URL_HASH}")
+      list(APPEND REQUESTED_VERSION ${args_URL_HASH})
+    endif()
+  else()
+    message(FATAL_ERROR "No Git repository or download URL declared as source")
   endif()
 
   # Download immediately if necessary
@@ -75,10 +94,19 @@ function(external_download TARGET)
 
     # Set cached version
     external_set_property(${TARGET} BUILD_TYPE "${CMAKE_BUILD_TYPE}")
-    external_set_property(${TARGET} GIT_REPOSITORY ${args_GIT_REPOSITORY})
 
-    if(args_GIT_TAG)
-      external_set_property(${TARGET} GIT_TAG ${args_GIT_TAG})
+    if(args_GIT_REPOSITORY)
+      external_set_property(${TARGET} GIT_REPOSITORY ${args_GIT_REPOSITORY})
+
+      if(args_GIT_TAG)
+        external_set_property(${TARGET} GIT_TAG ${args_GIT_TAG})
+      endif()
+    elseif(args_URL)
+      external_set_property(${TARGET} URL ${args_URL})
+
+      if(args_URL_HASH)
+        external_set_property(${TARGET} URL_HASH ${args_URL_HASH})
+      endif()
     endif()
 
     external_set_typed_property(${TARGET} NEW_VERSION TRUE BOOL)
